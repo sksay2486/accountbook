64980f77500a2bcc4d4a5dfc8f678804dc5d6d46
kwlee <sksay2486@naver.com>
2022-10-07 10:48:20 +0900

    tailwind 적용

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-export default function Home() {
+export default function Main() {
   return (
-    <></>
+    <div className="head-style">
+      <div className="title-style">
+        2022년 10월
+      </div>
+      <div className="notice-style">
+        <ul>
+          <li>11111111</li>
+          <li>22222222</li>
+          <li>33333333</li>
+        </ul>
+      </div>
+    </div>
   )
 }
